d9c550e35c2e4428bf537ead19b9d5f29121e55c
37CARE <mhamed@37care.co>
2019-04-08 22:02:38 +0200

    troisieme sauvegarde

---
+++ lib/03_basic.rb
@@ -0,0 +1,20 @@
+def who_is_bigger(a,b,c)
+    if a == nil || b == nil || c == nil
+      puts "nil detected"
+    elsif (a >= b) && (a >= c)
+        # number = [a,b,c]
+        # puts number.sort[2] "is bigger"
+        puts "a is bigger"
+    elsif (b >= a) && (b >= c)
+        puts "b is bigger"
+    elsif (c >= a) && (c >= b)
+        puts "c is bigger"
+    end
+end
+ puts who_is_bigger(84,4000,nil)
+
+ def crazy_stuff(a)
+    return a.to_s.upcase.reverse!
+ end
+ puts crazy_stuff("Tries this at Home, Kids")
+ 
